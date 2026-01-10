490d99c2042a55226911398f4a741c613d45e4c0
ikandelaki <iraklikandelaki2@gmail.com>
2026-01-10 21:06:52 +0400

    Set up tanstack query provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Notifications from "@/components/Notification/Notification";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Notifications />
+        <QueryProvider>
+          {children}
+          <Notifications />
+        </QueryProvider>
       </body>
     </html>
   );
