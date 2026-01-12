5b216dc2f786e77ea95cdd8bba2f1dae9d74964b
ikandelaki <iraklikandelaki2@gmail.com>
2026-01-12 20:02:03 +0400

    Implement loading skeletons, for now only for menu edit page

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -6,7 +6,9 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     return (
         <div className='AdminLayout flex'>
             <Navbar />
-            { children }
+            <div className="py-2 px-8 w-full">
+                { children }
+            </div>
         </div>
     )
 }
---
+++ frontend/src/app/admin/menus/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div>
+            <div className="skeleton w-22 h-5"></div>
+            <div className="skeleton w-16 h-5"></div>
+        </div>
+    );
+}
