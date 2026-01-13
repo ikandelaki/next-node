0f3e926d380e9362827e3174cc20d8ea2514df70
ikandelaki <iraklikandelaki2@gmail.com>
2026-01-13 16:50:53 +0400

    Remove server directory as it is not needed after installing prisma

--- server/README.md
+++
@@ -1,2 +0,0 @@
-node version: ^20.0.0
-mysql version: ^8.0.44
--- server/repository/MenuRepository.ts
+++
@@ -1,9 +0,0 @@
-import BaseRepository from "./BaseRepository.js";
-
-export default class MenuRepository extends BaseRepository {
-    constructor() {
-        super();
-
-        this.tableName = 'menu';
-    }
-}
