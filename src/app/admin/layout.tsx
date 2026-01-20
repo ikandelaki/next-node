cd9f5d64441f7cb6056c90d91b257a0bb0802421
Irakli Kandelaki <iraklikandelaki2@gmail.com>
2026-01-20 20:25:41 +0400

    Create (public) route group to separate public and admin routes and logic

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,37 @@
-import Navbar from '@/components/Navbar/Navbar';
-import React from 'react';
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import Notifications from "@/components/Notification/Notification";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
+import Navbar from "@/components/Navbar";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: '400'
+});
+
+export const metadata: Metadata = {
+  title: "Admin",
+  description: "Admin dashboard",
+};
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
     return (
-        <div className='flex'>
-            <Navbar />
-            <div className="py-2 px-8 w-full">
-                { children }
-            </div>
-        </div>
-    )
+        <html lang="en">
+            <body className={`${poppins.variable} antialiased`}>
+                <div className="flex">
+                    <Navbar />
+                    <div className="py-2 px-8 w-full">
+                        { children }
+                    </div>
+                    <Notifications />
+                </div>
+            </body>
+        </html>
+    );
 }
