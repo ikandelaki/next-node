cf98218d1a376a8e6c0d80a19df06d904559f318
ikandelaki <iraklikandelaki2@gmail.com>
2026-01-24 19:47:42 +0400

    Convert Navbar into client component because I need to make sidebar into expandable sidebar, so I need the state to mark sidebar as expanded or not expanded

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,64 @@
+'use client';
+
 import Link from 'next/link';
 import MenuIcon from '@/components/MenuIcon';
 import DashboardIcon from '@/components/DashboardIcon';
 import CubeIcon from '../CubeIcon';
+import ChevronIcon from '../ChevronIcon';
+import { useState } from 'react';
+
+type MenuItemType = {
+    title: string;
+    to?: string,
+    icon: React.ReactNode,
+    isExpandable?: boolean,
+    children?: { [key: string]: MenuItemType[] }
+}
 
 export default function Navbar() {
-    const renderMap = [
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const renderMap: MenuItemType[] = [
         {
             title: 'Dashboard',
             to: 'dashboard',
-            icon: <DashboardIcon />
+            icon: <DashboardIcon />,
+            isExpandable: false
         },
         {
             title: 'Menu manager',
             to: 'menus',
-            icon: <MenuIcon />
+            icon: <MenuIcon />,
+            isExpandable: false
         },
         {
-            title: "Products",
-            to: 'products',
-            icon: <CubeIcon />
+            title: "Content",
+            to: '',
+            icon: <CubeIcon />,
+            isExpandable: true,
+            children: {
+                'Entities': [
+                    {
+                        title: "Products",
+                        to: 'products',
+                        icon: <CubeIcon />,
+                        isExpandable: true,
+                    }
+                ]
+            }
         }
     ];
 
-    const renderNavLinks = () => {
-        return (
-            <ul className="flex flex-col gap-8">
-            { renderMap.map(({ title, to, icon }, key) => (
+    const handleButtonClick = () => {
+        setIsExpanded(!isExpanded);
+    }
+
+    const renderNavLink = (
+        { title, to, icon, isExpandable, children }: MenuItemType,
+        key: number
+    ) => {
+        if (!isExpandable) {
+            return (
                 <li key={ `${to}-${key}` }>
                     <Link href={ `/admin/${to}` } className="flex w-max gap-2 items-center">
                         <div className="w-5 h-5">
@@ -36,13 +69,34 @@ export default function Navbar() {
                         </div>
                     </Link>
                 </li>
-            )) }
+            )
+        }
+
+        return (
+            <button
+                className='flex w-full gap-2 items-center cursor-pointer'
+                key={ `${title}-${key}` }
+                onClick={ handleButtonClick }
+            >
+                { icon && icon }
+                <span className="cursor-pointer">{ title }</span>
+                <span className='rotate-270 ml-auto'>
+                    <ChevronIcon />
+                </span>
+            </button>
+        )
+    }
+
+    const renderNavLinks = () => {
+        return (
+            <ul className="flex flex-col gap-8">
+                { renderMap.map((menu, key) => renderNavLink(menu, key)) }
             </ul>
         );
     }
 
     return (
-        <div className="bg-navbar border-r border-line h-screen p-6">
+        <div className={`bg-navbar border-r border-line h-screen p-6 grid grid-rows-[${isExpanded ? '2' : '1'}fr]`}>
             { renderNavLinks() }
         </div>
     );
