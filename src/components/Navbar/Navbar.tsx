353f452023f29f4e1751ab83ebf79ecbbb712e6c
ikandelaki <iraklikandelaki2@gmail.com>
2026-01-24 21:23:10 +0400

    Create logic to have expandable menu items, it is kind of working, but could be better

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import MenuIcon from '@/components/MenuIcon';
 import DashboardIcon from '@/components/DashboardIcon';
 import CubeIcon from '../CubeIcon';
 import ChevronIcon from '../ChevronIcon';
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
 
 type MenuItemType = {
     title: string;
@@ -16,7 +16,8 @@ type MenuItemType = {
 }
 
 export default function Navbar() {
-    const [isExpanded, setIsExpanded] = useState(false);
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const [expandedMenuId, setExpandedMenuId] = useState<number | null>(null);
 
     const renderMap: MenuItemType[] = [
         {
@@ -42,15 +43,22 @@ export default function Navbar() {
                         title: "Products",
                         to: 'products',
                         icon: <CubeIcon />,
-                        isExpandable: true,
+                        isExpandable: false,
                     }
                 ]
             }
         }
     ];
 
-    const handleButtonClick = () => {
+    const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+        const index = event.currentTarget?.dataset?.index;
+
+        if (!index) {
+            return;
+        }
+
         setIsExpanded(!isExpanded);
+        setExpandedMenuId(parseInt(index));
     }
 
     const renderNavLink = (
@@ -61,7 +69,7 @@ export default function Navbar() {
             return (
                 <li key={ `${to}-${key}` }>
                     <Link href={ `/admin/${to}` } className="flex w-max gap-2 items-center">
-                        <div className="w-5 h-5">
+                        <div className="w-6 h-6">
                             { icon && icon }
                         </div>
                         <div>
@@ -77,6 +85,7 @@ export default function Navbar() {
                 className='flex w-full gap-2 items-center cursor-pointer'
                 key={ `${title}-${key}` }
                 onClick={ handleButtonClick }
+                data-index={ key }
             >
                 { icon && icon }
                 <span className="cursor-pointer">{ title }</span>
@@ -89,15 +98,45 @@ export default function Navbar() {
 
     const renderNavLinks = () => {
         return (
-            <ul className="flex flex-col gap-8">
+            <ul className="flex flex-col gap-8 bg-navbar">
                 { renderMap.map((menu, key) => renderNavLink(menu, key)) }
             </ul>
         );
     }
 
+    const renderSection = (title: string, children: { [key:string]: MenuItemType[] }, key = 0) => {
+        return (
+            <ul key={ `${title}-${key}` }>
+                <h2 className='mb-2'>{ title }</h2>
+                { children[title].map(
+                    (content: MenuItemType, key: number) => renderNavLink(content, key)
+                ) }
+            </ul>
+        );
+    }
+
+    const renderSections = (children: { [key:string]: MenuItemType[] }) => {
+        return Object.keys(children).map(
+            (title, key) => renderSection(title, children, key)
+        )
+    }
+
+    const renderExpandableSidebarContent = () => {
+        const menu = renderMap[expandedMenuId || 0];
+
+        const { children = {} } = menu || {};
+        return (
+            <div className={`absolute w-full left-full top-0 bg-navbar h-full p-6
+                transition-all duration-300 ${isExpanded ? 'translate-x-0 shadow-2xl border-r border-line' : '-translate-x-full border-none'} -z-9`}>
+                { renderSections(children) }
+            </div>
+        )
+    }
+
     return (
-        <div className={`bg-navbar border-r border-line h-screen p-6 grid grid-rows-[${isExpanded ? '2' : '1'}fr]`}>
+        <div className={`bg-navbar border-r border-line h-screen p-6 relative z-9 shadow-2xl`}>
             { renderNavLinks() }
+            { renderExpandableSidebarContent() }
         </div>
     );
 }
