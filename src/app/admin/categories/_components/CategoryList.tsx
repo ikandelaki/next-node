56414db60b58790d686f81ca2e413a0588ed8aee
ikandelaki <iraklikandelaki2@gmail.com>
2026-02-06 18:09:21 +0400

    Render category list as a list, need to create logic to render category list as a tree

--- src/app/admin/categories/_components/CategoryList.tsx
+++ src/app/admin/categories/_components/CategoryList.tsx
@@ -1,3 +1,46 @@
+import { Category } from "@/app/generated/prisma/client";
+import prisma from "@/lib/prisma";
+
 export default async function CategoryList() {
-  return <section className="Section">Category list</section>;
+  const categories = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+      children: true,
+      parentId: true,
+    },
+  });
+
+  type CategoryListItem = Pick<Category, "id" | "name" | "parentId"> & {
+    children: Category[];
+  };
+
+  const renderCategoryListItem = (category: CategoryListItem) => {
+    return (
+      <li key={category.id} className="mt-2 list-disc">
+        {category.name}
+      </li>
+    );
+  };
+
+  const renderCategoryList = () => {
+    if (!categories || !categories?.length) {
+      return <span>You have no categories...</span>;
+    }
+
+    return (
+      <ul>
+        {categories.map((category) =>
+          renderCategoryListItem(category as CategoryListItem),
+        )}
+      </ul>
+    );
+  };
+
+  return (
+    <section className="Section">
+      <h2>Category list</h2>
+      {renderCategoryList()}
+    </section>
+  );
 }
